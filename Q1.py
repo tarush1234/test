af1700269b2fa4120b2032fbf2e5abd6f702a961
tarushgupta <85294591+tarush1234@users.noreply.github.com>
2021-06-05 15:04:28 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,9 @@
+print('''AR and VR
+full form of AR is arrogation reality 
+and full form of VR is virtual reality
+AR is a machine from which you can appear any thing  where you are sitting
+where as in VR it is used to go in a other world
+ex of VR:
+play games
+ex of AR:
+to recored movies''')
